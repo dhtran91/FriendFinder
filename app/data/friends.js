2b9fc25484ff429236c9fbee9104ab473603e903
David Tran <dhtran@hotmail.com>
2018-07-31 21:08:33 -0500

    Done

--- app/data/friends.js
+++ app/data/friends.js
@@ -7,7 +7,8 @@ function Friend(name, photo, scores){
 let friends = [];
 
 //Dummy data
-friends.push(new Friend("Meghan", "https://s24193.pcdn.co/wp-content/uploads/2017/07/rachel-zane-suits-entity-meghan-markle-1320x720.png", [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]))
+friends.push(new Friend("Meghan", "https://s24193.pcdn.co/wp-content/uploads/2017/07/rachel-zane-suits-entity-meghan-markle-1320x720.png", [5,5,3,3,3,3,3,3,3,3]))
+friends.push(new Friend("Obama", "https://upload.wikimedia.org/wikipedia/commons/f/f5/Poster-sized_portrait_of_Barack_Obama.jpg", [1,1,1,1,1,1,1,1,2,2]))
 
 module.exports = friends;
 module.exports.newFriend = Friend;
